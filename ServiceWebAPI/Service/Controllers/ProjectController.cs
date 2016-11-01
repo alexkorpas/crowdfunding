56a2de5bf1f3d348d81b496a1ba99b8d850e0661
Alexandros Ginakos <ginalx@hotmail.com>
2016-11-02 00:01:41 +0200

    Second commit, setting up the structure

---
+++ ServiceWebAPI/Service/Controllers/ProjectController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Service
+{
+    public class ProjectController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage GetProjects()
+        {
+            try
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.OK, "success");
+            }
+            catch (Exception e) { return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message); }
+        }
+    }
+}

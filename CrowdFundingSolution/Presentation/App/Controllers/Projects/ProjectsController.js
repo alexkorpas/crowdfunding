1e52d65c6ec554a6ff13d549374694e33a9b2c01
gemakada <gemakada@gmail.com>
2016-11-17 00:24:52 +0200

    Pagination_test

--- CrowdFundingSolution/Presentation/App/Controllers/Projects/ProjectsController.js
+++ CrowdFundingSolution/Presentation/App/Controllers/Projects/ProjectsController.js
@@ -1,13 +1,31 @@
 ﻿'use strict';
 CrowdFundingApp.controller('ProjectsController', ['$scope', '$state', 'ngDialog', '$filter', '$element', '$http', '$stateParams', 'baseService',
     function ($scope, $state, ngDialog, $filter, $element, $http, $stateParams, baseService) {
-        baseService.httpGetAnonymous("api/Project/ReadAndCount", null).then(function (res) {
-           // $scope.Projects = res;
+        baseService.httpGetAnonymous("api/Project/GetPageCount", null).then(function (res) {
+            var pages; 
+            var pageindex=[];
+            if (angular.isNumber(res)) {
+                console.log(res);
+                var rem = res % 3;
+                if (rem != 0) {
+                    pages = (Math.floor(res/3)) + 1;
+                }
+                else {
+                    pages = res;
+                }
+                
+                
+                console.log(pages);
+                for (var i=0; i<pages; i++) {
+                    pageindex.push(i+1);
+                }
+                $scope.index=pageindex;
+            }
             
             var count = Object.keys(res).length;
-            console.log(res.Table_Size);
+            //console.log(res);
         });
-        baseService.httpGetAnonymous("api/Project/GetProjects", null).then(function (res) {
+        baseService.httpGetAnonymous("api/Project/GetProjects/?Page=1", null).then(function (res) {
              $scope.Projects = res;
 
             var count = Object.keys(res).length;

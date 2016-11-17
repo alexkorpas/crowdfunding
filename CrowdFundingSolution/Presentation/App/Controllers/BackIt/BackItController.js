f435aa0bbb3d640a4f6af08fb81b9a88c70def6e
Alexandros Ginakos <ginalx@hotmail.com>
2016-11-18 00:21:15 +0200

    payment is real

--- CrowdFundingSolution/Presentation/App/Controllers/BackIt/BackItController.js
+++ CrowdFundingSolution/Presentation/App/Controllers/BackIt/BackItController.js
@@ -1,13 +1,15 @@
 ﻿'use strict';
-CrowdFundingApp.controller('BackItController', ['$scope', '$state', 'ngDialog', '$filter', '$element', '$http', '$stateParams', 'authService', 'baseService', 'CFHelpers',
-    function ($scope, $state, ngDialog, $filter, $element, $http, $stateParams, authService, baseService, CFHelpers) {
+CrowdFundingApp.controller('BackItController', ['$scope', '$state', 'ngDialog', '$filter', '$element', '$http', '$stateParams', 'authService', 'baseService', 'CFHelpers', '$q', 'servers',
+    function ($scope, $state, ngDialog, $filter, $element, $http, $stateParams, authService, baseService, CFHelpers, $q, servers) {
         VivaPayments.cards.setup({
             publicKey: 'yZyg05StUwLG3QnfY2bY6fAfGsxPVawXI5etOxbNQ7E=',
             baseURL: 'http://demo.vivapayments.com',
             cardTokenHandler: function (response) {
                 if (!response.Error) {
-                    $('#hidToken').val(response.Token);
-                    $('#payment-form').submit();
+                    //$('#hidToken').val(response.Token);
+                    $scope.hidToken = response.Token;
+                    //$('#payment-form').submit();
+                    $scope.test();
                 }
                 else
                     alert(response.Error);
@@ -28,14 +30,19 @@ CrowdFundingApp.controller('BackItController', ['$scope', '$state', 'ngDialog', 
         $scope.test = function () {
 
             var deferred = $q.defer();
-            var config = {
-                method: 'POST',
-                data: 'json',
-                params: $scope.hidToken
-            };
-            $http.post(
-                servers.CF_SERVER + URI, config
-                ).success(function (data) {
+            //var config = {
+            //    method: 'POST',
+            //    data: 'json'//,
+            //    //params: $scope.hidToken
+            //};
+            //config.params = { ourToken: $scope.hidToken };
+            $http({
+                url: servers.CF_SERVER + 'api/Payment/Pay',
+                method: "POST",
+                params: { ourToken: $scope.hidToken },
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                skipAuthorization: true
+            }).success(function (data) {
                     deferred.resolve(data);
                 }).error(function (err, status) {
                     deferred.reject(err);

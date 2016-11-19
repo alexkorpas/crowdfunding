b8f3982f66774d1a1d87d4964eafa423a1af8542
dvs49 <dimitrisvalasidis@hotmail.com>
2016-11-19 19:02:45 +0200

    profile works( one way)

--- CrowdFundingSolution/Presentation/App/Controllers/Profile/ProfileController.js
+++ CrowdFundingSolution/Presentation/App/Controllers/Profile/ProfileController.js
@@ -1,15 +1,7 @@
 ﻿'use strict';
 CrowdFundingApp.controller('ProfileController', ['$scope', '$state', 'ngDialog', '$filter', '$element', '$http', '$stateParams', 'authService', 'baseService', 'CFHelpers',
     function ($scope, $state, ngDialog, $filter, $element, $http, $stateParams, authService, baseService, CFHelpers) {
-        baseService.httpGet("api/Project/GetLoggedInUser".then(function (res) {
-            $scope.user= res;
-        }));
-        //$scope.backIt = function () {
-        //    var dialog = ngDialog.open({ // ngDialog
-        //        template: 'App/Views/BackIt/BackItForm.html',
-        //        className: 'ngdialog-theme-default',
-        //        controller: 'BackItController',
-        //        scope: $scope
-        //    });
-        //};
+        baseService.httpGet("api/User/GetLoggedInUser",null).then(function (res) {
+            $scope.User = res;
+        });
     }]);

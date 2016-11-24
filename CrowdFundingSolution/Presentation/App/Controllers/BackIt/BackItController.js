9cb58b8f5f569b1c8811a6648d280e6e619bd69b
Alexandros Ginakos <ginalx@hotmail.com>
2016-11-24 02:51:10 +0200

    payment form

--- CrowdFundingSolution/Presentation/App/Controllers/BackIt/BackItController.js
+++ CrowdFundingSolution/Presentation/App/Controllers/BackIt/BackItController.js
@@ -1,33 +1,38 @@
 ﻿'use strict';
-CrowdFundingApp.controller('BackItController', ['$scope', '$state', 'ngDialog', '$filter', '$element', '$http', '$stateParams', 'authService', 'baseService', 'CFHelpers', '$q', 'servers',
-    function ($scope, $state, ngDialog, $filter, $element, $http, $stateParams, authService, baseService, CFHelpers, $q, servers) {
-        $scope.$on('$viewContentLoaded', function () {
+CrowdFundingApp.controller('BackItController', ['$scope', '$state', 'ngDialog', '$filter', '$element', '$http', '$stateParams', 'authService', 'baseService', 'CFHelpers', '$q', 'servers', '$mdToast',
+    function ($scope, $state, ngDialog, $filter, $element, $http, $stateParams, authService, baseService, CFHelpers, $q, servers, $mdToast) {
+        //$scope.$on('$viewContentLoaded', function () {
+        setTimeout(function(){ 
             VivaPayments.cards.setup({
                 publicKey: 'yZyg05StUwLG3QnfY2bY6fAfGsxPVawXI5etOxbNQ7E=',
                 baseURL: 'http://demo.vivapayments.com',
                 cardTokenHandler: function (response) {
                     if (!response.Error) {
                         $scope.hidToken = response.Token;
-                        $scope.test();
+                        $scope.pay();
                     }
                     else
                         alert(response.Error);
                 },
-                installmentsHandler: function (response) {
-                    if (!response.Error) {
-                        if (response.MaxInstallments == 0)
-                            return;
-                        $('#drpInstallments').show();
-                        for (let i = 1; i <= response.MaxInstallments; i++) {
-                            $('#drpInstallments').append($("<option>").val(i).text(i));
-                        }
-                    }
-                    else
-                        alert(response.Error);
-                }
+                //installmentsHandler: function (response) {
+                //    if (!response.Error) {
+                //        if (response.MaxInstallments == 0)
+                //            return;
+                //        $('#drpInstallments').show();
+                //        for (let i = 1; i <= response.MaxInstallments; i++) {
+                //            $('#drpInstallments').append($("<option>").val(i).text(i));
+                //        }
+                //    }
+                //    else
+                //        alert(response.Error);
+                //}
             });
-            $scope.test = function () {
+            $scope.submitPayment = function () {
+                VivaPayments.cards.requestToken();
+            };
+            $scope.pay = function () {
                 var deferred = $q.defer();
+                $scope.backIt.closeDialog();
                 $http({
                     url: servers.CF_SERVER + 'api/Payment/Pay',
                     method: "POST",
@@ -35,11 +40,25 @@ CrowdFundingApp.controller('BackItController', ['$scope', '$state', 'ngDialog', 
                     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
                     skipAuthorization: true
                 }).success(function (data) {
-                    deferred.resolve(data);
+                    deferred.resolve(data);                    
+                    $mdToast.show(
+                                  $mdToast.simple()
+                                    .textContent("Transasction Successful")
+                                    .position('top right')
+                                    .hideDelay(3000)
+                                    );
                 }).error(function (err, status) {
                     deferred.reject(err);
+                    $mdToast.show(
+                                  $mdToast.simple()
+                                    .textContent(err.Message)
+                                    .position('top right')
+                                    .hideDelay(3000)
+                                    .toastClass('failure')
+                                    );
                 });
                 return deferred.promise;
             };
-        });
+        }, 500);
+        //});
     }]);
--- CrowdFundingSolution/Presentation/App/Controllers/Packages/PackageControler.js
+++ CrowdFundingSolution/Presentation/App/Controllers/Packages/PackageControler.js
@@ -4,4 +4,26 @@ CrowdFundingApp.controller('PackageController', ['$scope', '$state', 'ngDialog',
         baseService.httpGetAnonymous("api/ProjectDetails/GetProjectFundingLevels", { Id: $stateParams.Id }).then(function (res) {
             $scope.Packages = res;
         });
+        $scope.showDiv = function (id) {
+            var div = angular.element(document.querySelector("#div" + id));
+            div.show();
+        };
+        $scope.backIt = function () {
+            var dialog = ngDialog.open({ // ngDialog
+                template: 'App/Views/BackIt/BackItForm.html',
+                className: 'ngdialog-theme-default',
+                controller: 'BackItController',
+                scope: $scope
+            });
+            $scope.backIt.closeDialog = function () {
+                dialog.close();
+            };
+        };
+        $scope.isOpen = false;
+
+        $scope.demo = {
+            isOpen: false,
+            count: 0,
+            selectedDirection: 'left'
+        };
     }]);

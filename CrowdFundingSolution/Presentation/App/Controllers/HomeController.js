2696c6fbd8169fc59ceeb22cba165c7cf4181cbc
Alexandros Ginakos <ginalx@hotmail.com>
2016-11-26 19:55:46 +0200

    Search Development, Nav Bar improvements

--- CrowdFundingSolution/Presentation/App/Controllers/HomeController.js
+++ CrowdFundingSolution/Presentation/App/Controllers/HomeController.js
@@ -1,6 +1,6 @@
 ﻿'use strict';
-CrowdFundingApp.controller('HomeController', ['$scope', '$state', 'ngDialog', '$filter', '$element', '$http', '$stateParams', 'CFHelpers', 'CFConfig',
-    function ($scope, $state, ngDialog, $filter, $element, $http, $stateParams, CFHelpers, CFConfig) {
+CrowdFundingApp.controller('HomeController', ['$scope', '$state', 'ngDialog', '$filter', '$element', '$http', '$stateParams', 'CFHelpers', 'CFConfig', 'baseService',
+    function ($scope, $state, ngDialog, $filter, $element, $http, $stateParams, CFHelpers, CFConfig, baseService) {
         $scope.logout = function () {
             CFHelpers.deleteToken();
             CFConfig.LOGUSER = "NULL"
@@ -10,9 +10,21 @@ CrowdFundingApp.controller('HomeController', ['$scope', '$state', 'ngDialog', '$
             console.log(field);
             $state.go("Home.Projects",{Search:field});
         }
-        //$scope.test = function () {
-        //    alert("hedas");
-        //};
+        $scope.searchBtn = function () {
+            baseService.httpGetAnonymous("api/Project/GetProjectCategories", null).then(function (res) {
+                $scope.SelectCategories = res;
+            });
+            var dialog = ngDialog.open({ // ngDialog
+                template: 'App/Views/SearchDialog.html',
+                className: 'search ngdialog-theme-default',
+                //controller: 'BackItController',
+                scope: $scope
+            });
+            setTimeout(function () {
+                var input = angular.element(document.querySelector("#searchInput"));
+                input.focus();
+            }, 200);
+        };
     }
     
 ]);
